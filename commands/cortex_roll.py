db24577352c1b67cfc1130d415539c13d1efbb46
EmpireMU <empiremux@gmail.com>
2025-05-13 23:58:46 +0100

    test

---
+++ commands/cortex_roll.py
@@ -0,0 +1,37 @@
+from evennia import Command
+from evennia import CmdSet
+from random import randint
+
+
+class CmdCortexRoll(Command):
+    """
+    A command to roll dice.
+    """
+    
+    key = "roll"
+
+    def func(self):
+        args = self.args.strip
+        if not args:
+            self.caller.msg("What dice do you want to roll?")
+            return
+        dice = args.split("+")
+        results = []
+        i = 0
+        for die in dice:
+            i += 1
+            results[i] = randint(1, die)
+            return
+        
+
+
+
+        caller = self.caller
+        location = caller.location
+        message = results
+        location.msg_contents(message)
+    
+
+class CortexCmdSet(CmdSet):
+    def at_cmdset_creation(self):
+        self.add(CmdCortexRoll)
